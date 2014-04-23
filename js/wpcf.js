6f2170b2c36fa53d6d935d6e8a7b229200820900
zzxuanran <Rong.Zou@stonybrook.edu>
2014-04-23 13:32:51 -0400

    Revert "version 1.1 initial commit"
    This reverts commit d2f2b5ee778c65f03d188c300bc5568684deb287.

--- js/wpcf.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {	
-	$(".wpcf7-form-control-wrap").mouseover(function(){
-		$obj = $("span.wpcf7-not-valid-tip",this);
-    	        $obj.css('display','none');
-	});
-});
